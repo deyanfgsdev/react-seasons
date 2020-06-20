641b66067b3d6a12affdb1cb37d1fcda8080a904
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-06-20 13:55:24 +0100

    update state properties and handling errors

--- src/index.js
+++ src/index.js
@@ -5,20 +5,35 @@ import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { latitude: null }; // Initialize 'State'
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    // Initialize 'State'
+    this.state = {
+      latitude: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        // we called setState!!!
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
   }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (error) => {
-        console.log(error);
-      }
+    return (
+      <div>
+        Latitude: {this.state.latitude}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
     );
-    return <div>Latitude: </div>;
   }
 }
 
